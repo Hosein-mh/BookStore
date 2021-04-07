fa9f32185723f91720a3ad610c769c50eb732adb
Hossein Mohamadi <homoh817@gmail.com>
2021-04-07 12:09:47 +0430

    registers models to django-admin

--- bookstore/books/admin.py
+++ bookstore/books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from books.models import Book, Category, Author, Book_Author
+
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Book_Author)
+
