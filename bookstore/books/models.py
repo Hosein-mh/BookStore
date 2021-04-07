870f526e34121483bb18babffa8a7683251552fa
Hossein Mohamadi <homoh817@gmail.com>
2021-04-07 11:44:21 +0430

    initial Book and Category model

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+  def __str__(self):
+    return self.title
+
+  title = models.CharField(max_length=255)
+  price = models.IntegerField(default=0)
+  category = models.ForeignKey('Category', on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+  updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+class Category(models.Model):
+  def __str__(self):
+    return self.name
+  
+  name = models.CharField(max_length=255)
+  parent = models.ForeignKey('Category', on_delete=models.PROTECT,
+                    related_name='children', blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+  updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+  
+  
